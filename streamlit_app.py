b6a10fe84b9182972e420526f9804740f2d5ee68
shardulkumar <shardulkumar@live.in>
2024-07-04 21:15:24 +0530

    update app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-st.title("Zena's Amazing Athleisure App")
-st.text("test app")
+st.title("Zena's Amazing Athleisure Catalog")
+st.selectbox("Pick a sweatsuit color or style")
