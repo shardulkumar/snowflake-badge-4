56d2756586857dcd98fc83449adcab6f9fe9989b
shardulkumar <shardulkumar@live.in>
2024-07-04 21:26:51 +0530

    update app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,27 @@
 import streamlit as st
+from snowflake.snowpark.context import get_active_session
 
-st.title("Zena's Amazing Athleisure Catalog")
-st.selectbox("Pick a sweatsuit color or style")
+
+def main():
+    return None
+
+
+def render_elements():
+    # Get session
+    session = get_active_session()
+
+    # Render title
+    st.title("Zena's Amazing Athleisure Catalog")
+
+    # Set the dropdown with relevant data
+    available_colors = session.sql("""
+        select color_or_style
+        from ZENAS_ATHLEISURE_DB.PRODUCTS.CATALOG_FOR_WEBSITE
+    """).to_pandas()
+    st.selectbox("Pick a sweatsuit color or style:", available_colors)
+
+    return None
+
+
+if __name__ == '__main__':
+    main()
