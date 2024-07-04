a55898d6887532a46b84d2826e8ae01e0f2947bd
shardulkumar <shardulkumar@live.in>
2024-07-04 23:53:05 +0530

    update app

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,23 +25,27 @@ def render_elements():
     )
 
     # Get image_link, size etc from catalog view
-    catalog_data = session.sql(
-        """
-        select
-              file_name
-            , file_url
-            , price
-            , size_list
-            , upsell_product_desc
-        from catalog_for_website
-        where color_or_style = '{selected_color}'
-        """.format(selected_color=selected_option)
-    ).to_pandas()
+    # catalog_data = session.sql(
+    #     """
+    #     select
+    #           file_name
+    #         , file_url
+    #         , price
+    #         , size_list
+    #         , upsell_product_desc
+    #     from catalog_for_website
+    #     where color_or_style = '{selected_color}'
+    #     """.format(selected_color=selected_option)
+    # ).to_pandas()
 
-    file_url = catalog_data['FILE_URL'].iloc[0]
-    price = '$' + str(catalog_data['PRICE'].iloc[0]) + '0'
-    size_list = catalog_data['SIZE_LIST'].iloc[0]
-    upsell = catalog_data['UPSELL_PRODUCT_DESC'].iloc[0]
+    prod_data = session.table('catalog_for_website') \
+        .select(['FILE_NAME', 'FILE_URL', 'PRICE', 'SIZE_LIST', 'UPSELL_PRODUCT_DESC']) \
+        .where(col('COLOR_OR_STYLE') == selected_option)
+
+    file_url = prod_data['FILE_URL'].iloc[0]
+    price = '$' + str(prod_data['PRICE'].iloc[0]) + '0'
+    size_list = prod_data['SIZE_LIST'].iloc[0]
+    upsell = prod_data['UPSELL_PRODUCT_DESC'].iloc[0]
 
     product_caption = "Our warm, comfortable, {color} sweatsuit!" \
         .format(color=selected_option)
